ff59730f3e896c7be962d31a17043da45c3e3228
Dlopez079 <dlopez@lopezdavid.dev>
2020-04-16 14:40:54 -0400

    Disabled formatting so I am able to type freely

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,7 +2,9 @@
   <div>
     <h1>Events Listings</h1>
     <p>
-      <router-link :to="{ name: 'event-show', params: { id: '1' } }">First Event</router-link>
+      <router-link :to="{ name: 'event-show', params: { id: '1' } }">
+        First Event</router-link
+      >
     </p>
   </div>
 </template>
