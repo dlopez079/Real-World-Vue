292ff6fdc4af24e3710a23e2305f476aed77c734
Dlopez079 <dlopez@lopezdavid.dev>
2020-04-16 19:14:44 -0400

    Remove router-link from EventList.vue

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -3,9 +3,6 @@
 	<div>
 		<h1>Events Listings</h1>
 		<EventCard></EventCard>
-		<p>
-			<router-link :to="{ name: 'event-show', params: { id: '1' } }">First Event</router-link>
-		</p>
 	</div>
 </template>
 
