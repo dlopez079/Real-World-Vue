7c06e506d9b09c1e697bbf8dc42c3d289c8883b8
Dlopez079 <dlopez@lopezdavid.dev>
2020-04-16 18:05:40 -0400

    Modified data to include event.

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,21 +1,33 @@
+/* eslint-disable */ 
 <template>
-  <div>
-    <h4>{{ title }}</h4>
-  </div>
+	<div class="event-card -shadow">
+		<span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+		<h4 class="title">{{ event.title }}</h4>
+		<span>{{ event.attendees.length }} attending</span>
+	</div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      title: "Park Cleanup"
-    };
-  }
+	data() {
+		return {
+			event: {
+				id: 1,
+				title: "Beach Cleanup",
+				date: "Tue May 15 2018",
+				time: "6:00",
+				attendees: [
+					{ id: "abc123", name: "Adam Jahr" },
+					{ id: "def456", name: "Gregg Pollack " }
+				]
+			}
+		};
+	}
 };
 </script>
 
 <style lang="css" scoped>
 h4 {
-  color: green;
+	color: green;
 }
 </style>
