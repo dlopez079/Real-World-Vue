dc1742a98ce6e1f9f10248b9332d86976855f0bb
Dlopez079 <dlopez@lopezdavid.dev>
2020-04-16 21:13:12 -0400

    Added users icon to EvenCard using global components

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -7,6 +7,7 @@
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
+      <baseIcon name="users" />
       <span>{{ event.attendees.length }} attending</span>
     </div>
   </router-link>
