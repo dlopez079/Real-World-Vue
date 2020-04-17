46250c5b9017db9d9ff70c94c5dab4c8754a0518
Dlopez079 <dlopez@lopezdavid.dev>
2020-04-17 19:09:06 -0400

    Created a Mediabox component.

---
+++ src/components/MediaBox.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<UserAvatar>
+			<slot></slot>
+			<slot></slot>
+		</UserAvatar>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
