84cfa531c06724ac3fcb348951dfacb189abba5e
Dlopez079 <dlopez@lopezdavid.dev>
2020-04-17 20:11:13 -0400

    Imported Axios Library and inserted create hook in EventList.vue

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,13 +1,24 @@
-/* eslint-disable */ 
+/* eslint-disable */
 <template>
-	<div>
-		<h1>Events Listings</h1>
-		<EventCard></EventCard>
-	</div>
+  <div>
+    <h1>Events Listings</h1>
+    <EventCard></EventCard>
+  </div>
 </template>
 
 <script>
 import EventCard from "../components/EventCard";
-export default { components: { EventCard: EventCard } };
+export default {
+  components: { EventCard: EventCard },
+  created() {
+    axios
+      .get("http://localhost:3000/events") // Does a get request
+      .then((response) => {
+        console.log(response.data); // For now, logs out the response
+      })
+      .catch((error) => {
+        console.log("There was an error:", error.response); // Logs out the error
+      });
+  },
+};
 </script>
-
